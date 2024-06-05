1b84111f4a3bd50f3e98627e737305c0bceff177
DAVI HENRY MOREL PINTOS <davi_pintos@estudante.sesisenai.org.br>
2024-06-05 11:30:05 -0300

    aa

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import { motion } from "framer-motion";
+import styled from "styled-components";
+import "./App.css";
+import { useState, useEffect } from "react";
+
+const Spinner = styled(motion.div)`
+  width: 50px;
+  height: 50px;
+  border: 4px solid black;
+  border-top: 4px solid red;
+  border-radius: 50% 50% 50% 50%;
+`;
+
+function App() {
+
+  const [Carregando, setCarregando] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setCarregando(false)
+    }, 2000)
+    return () => clearTimeout(timer)
+  }, [])
+
+  return (
+    <>
+      <div className="center">
+        { Carregando ? (<Spinner
+          animate={{ rotate: 360 }}
+          transition={{ duration: 1, repeat: Infinity, ease: "linear" }}
+        />):(
+          <h1>Content</h1>
+        )
+      }
+        
+      </div>
+    </>
+  );
+}
+
+export default App;
